e0abb904248c4c47b4917d47654105141731b6e9
Daniel Lee <danielyedarmlee@gmail.com>
2025-07-14 11:36:43 -0400

    Add onKeyDown event "Enter"

--- todo-app/src/components/TodoInput.jsx
+++ todo-app/src/components/TodoInput.jsx
@@ -4,20 +4,23 @@ export function TodoInput(prop) {
   const { handleAddTodo } = prop;
   const [inputValue, setInputValue] = useState(""); //search bar
 
+  function addTodo() {
+    if(!inputValue) return; //guard clause
+    handleAddTodo(inputValue);
+    setInputValue("");
+  }
+
   return (
     <div className="input-container">
       <input
         value={inputValue}
-        onChange={(event) => {
-          setInputValue(event.target.value);
+        onChange={(event) => setInputValue(event.target.value)}
+        onKeyDown={(event) => {
+          if(event.key === "Enter") {addTodo()}
         }}
         placeholder="Add Tasks"
       />
-      <button onClick={() => {
-        if(!inputValue) {return} //guard clause
-        handleAddTodo(inputValue) 
-        setInputValue('')
-      }}>
+      <button onClick={() => {addTodo()}}>
         <i className="fa-solid fa-plus"></i>
       </button>
     </div>
